48532b728b50e2bcc4dcaef44cba4f638f93f9b6
Igor Doslov <igordoslov@protonmail.com>
2021-12-09 16:41:21 +1100

    ID - ExplosiveBullet WIP

---
+++ Santas Revenge/Assets/Scripts/ExplosiveBullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ExplosiveBullet : MonoBehaviour
+{
+    public int damageAmount = 100;
+    public float radius = 5.0f;
+    public float explosionForce = 10000.0f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+
+            Health health = nearbyObject.GetComponent<Health>();
+
+
+            NavMeshAgent nav = nearbyObject.GetComponent<NavMeshAgent>();
+            if (nav != null)
+                nav.enabled = false;
+
+            if (rb != null && health != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, radius);
+                //Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+                //health.TakeDamage(damageAmount);
+            }
+        }
+
+        if (collision.rigidbody != null)
+            collision.rigidbody.AddForce(-collision.contacts[0].normal * 1000);
+
+        //gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+}
