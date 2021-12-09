2a8b3a4be323f5113fac7e952d6c238254835954
Justin <justinkatic4@gmail.com>
2021-12-09 16:40:03 +1100

    AI now choose random path wit move up

--- Santas Revenge/Assets/Scripts/EnemyMove.cs
+++ Santas Revenge/Assets/Scripts/EnemyMove.cs
@@ -6,15 +6,39 @@ using UnityEngine.AI;
 public class EnemyMove : MonoBehaviour
 {
     public NavMeshAgent agent;
-    Transform target;
 
-    private void Start()
+    private GameObject[] rampDestinations;
+    int rampDestIndex;
+    Transform player;
+
+    bool closeToPlayer = false;
+    float dist;
+
+    private void OnEnable()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        closeToPlayer = false;
+        rampDestIndex = Random.Range(0, rampDestinations.Length);
+        agent.SetDestination(rampDestinations[rampDestIndex].transform.position);
+    }
+
+    private void Awake()
+    {
+        rampDestinations = GameObject.FindGameObjectsWithTag("RampPos");
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void Update()
     {
-        agent.SetDestination(target.position);
+        if (!closeToPlayer)
+        {
+            dist = Vector3.Distance(transform.position, rampDestinations[rampDestIndex].transform.position);
+            if (dist <= 2f)
+            {
+                closeToPlayer = true;
+            }
+        }
+        else
+            agent.SetDestination(player.position);
+
     }
 }
