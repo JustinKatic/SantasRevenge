ced0a6263f19172a46242b432bc35907fad432d0
Kyle Gian <kyle.gian@hotmail.com>
2021-12-10 13:50:08 +1100

    Added attack script and player health

---
+++ Santas Revenge/Assets/Scripts/MinionAttack.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionAttack : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+        }
+    }
+}
