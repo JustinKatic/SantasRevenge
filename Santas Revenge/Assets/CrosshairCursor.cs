820b95ffcf57f4e32a4bd89e670cde6cb01315f2
Justin <justinkatic4@gmail.com>
2021-12-10 12:46:09 +1100

    cursor added

---
+++ Santas Revenge/Assets/CrosshairCursor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairCursor : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
